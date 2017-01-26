c5993e2bfc9fbb735cbeaafd09ac1e572f258b88
liamon <l.oneill20@nuigalway.ie>
2017-01-26 14:27:51 +0000

    Added basic schema to validate game tuples/documents. Can change later if needed.

--- collections/games.js
+++ collections/games.js
@@ -1,1 +1,9 @@
 Games = new Mongo.Collection('games');
+
+// May change the object type of some of these attributes later.
+Games.schema = new SimpleSchema({
+  "black": {type: String},
+  "white": {type: String},
+  "toPlay": {type: String},
+  "currentBoard": {type: [String]}
+});
