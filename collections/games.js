83f88d8c7830828cf3908e6cf778e0501c3ae4c6
liamon <l.oneill20@nuigalway.ie>
2017-01-30 13:50:35 +0000

    Changed player names from colors to numbers to have more choice of how to display the games.

--- collections/games.js
+++ collections/games.js
@@ -2,8 +2,8 @@ Games = new Mongo.Collection('games');
 
 // May change the object type of some of these attributes later.
 Games.schema = new SimpleSchema({
-  "black": {type: String},
-  "white": {type: String},
+  "player1": {type: String}, // I chose these names rather than colors to give
+  "player2": {type: String}, // us more flexibilty in how we display the game.
   "toPlay": {type: String},
   "currentBoard": {type: [String]}
 });
