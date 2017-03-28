659068c18cd8946a43ee049c5124765f49ed63bc
liamon <l.oneill20@nuigalway.ie>
2017-03-28 13:49:36 +0100

    More changes, mostly to do with spacing and indentation.

--- client/lib/main.js
+++ client/lib/main.js
@@ -1,51 +1,50 @@
 Meteor.subscribe('playerNames');
 
-if (Meteor.isClient){
-Meteor.subscribe('userPosts');
-Template.game.helpers({
+if (Meteor.isClient) {
+  Meteor.subscribe('userPosts');
+  Template.game.helpers({
     charsRemaining: function () {
-        return Session.get('CharactersRemaining');
+      return Session.get('CharactersRemaining');
     },
     posts : function () {
-        return Posts.find({}, {sort: {date: -1}});
+      return Posts.find({}, {sort: {date: -1}});
     },
     timeDiff : function (postDate) {
-        var timeDiff = new Date().getTime() - postDate.getTime();
-        var diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
-        var diffHours = Math.floor(timeDiff  / (1000 * 3600));
-        var diffMins = Math.floor(timeDiff  / (1000 * 60));
-        var diffSecs = Math.floor(timeDiff  / (1000));
+      var timeDiff = new Date().getTime() - postDate.getTime();
+      var diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+      var diffHours = Math.floor(timeDiff  / (1000 * 3600));
+      var diffMins = Math.floor(timeDiff  / (1000 * 60));
+      var diffSecs = Math.floor(timeDiff  / (1000));
 
-        if (diffDays > 0)
-             return ("about " + diffDays + "d ago");
-        else if(diffHours > 0)
-            return ("about " + diffHours +"h ago");
-        else if(diffMins > 0 )
-            return ("about " + diffMins + "m ago");
-        else if(diffSecs > 0)
-             return ("about " + diffSecs + "s ago");
-        },
-    userCreated : function (createdBy){
-        if(createdBy == Meteor.userId())
-            return true;
-        else
-            return false;
-    }
-});
-Template.game.onRendered(function () {
-    $("#postForm").validate();
-});
-Template.game.events({
-    'keyup #inputPost': function (event) {
-        var inputText = event.target.value;
-        Session.set("CharactersRemaining", (140 - inputText.length));
-
+      if (diffDays > 0)
+        return ("about " + diffDays + "d ago");
+      else if(diffHours > 0)
+        return ("about " + diffHours + "h ago");
+      else if(diffMins > 0 )
+        return ("about " + diffMins + "m ago");
+      else if(diffSecs > 0)
+        return ("about " + diffSecs + "s ago");
     },
-    'submit #postForm' : function(event){
-        event.preventDefault();
-        var post = event.target.inputPost.value;
+    userCreated : function (createdBy) {
+      if(createdBy == Meteor.userId())
+        return true;
+      else
+        return false;
+    }
+  });
+  Template.game.onRendered(function () {
+    $("#postForm").validate();
+  });
+  Template.game.events({
+    'keyup #inputPost': function (event) {
+      var inputText = event.target.value;
+      Session.set("CharactersRemaining", (140 - inputText.length));
+    },
+    'submit #postForm' : function(event) {
+      event.preventDefault();
+      var post = event.target.inputPost.value;
       $('#inputPost').val('');
-        Session.set("CharactersRemaining", (140) + "  "+ " Charaters Remaining");
+        Session.set("CharactersRemaining", 140 + "  " + " Charaters Remaining");
         Meteor.call('insertPost', post);
     },
     'click .editBox input' : function (event) {
@@ -53,19 +52,18 @@ Template.game.events({
         $('#edit'+this._id).removeClass('hidden');
         $('#post'+this._id).hide();
       }
-      else
-      {
-        var post = $('#edit'+this._id).val();
-        Meteor.call('updatePost', {id :this._id, post:post});
-        $('#edit'+this._id).addClass('hidden');
-        $('#post'+this._id).show();
+      else {
+        var post = $('#edit' + this._id).val();
+        Meteor.call('updatePost', {id : this._id, post: post});
+        $('#edit' + this._id).addClass('hidden');
+        $('#post' + this._id).show();
         $("h6").removeClass("test");
       }
     },
-    'click .deletePost button' : function(event){
+    'click .deletePost button' : function(event) {
         event.preventDefault();
         var curPostId = this._id;
         Posts.remove(curPostId);
     }
-    });
+  });
 }
--- server/main.js
+++ server/main.js
@@ -1,76 +1,78 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.publish('playerNames', function() {
-    // {} as the first argument of find will return all.
-    return Meteor.users.find({}, {fields: {username: 1}});
+  // {} as the first argument of find will return all.
+  return Meteor.users.find({}, { fields: { username: 1 } });
 });
 
 Meteor.startup(() => {
   // code to run on server at startup
 
-Meteor.methods({
-    'insertPost' : function(post){
-        Posts.insert(
-        {
-            post:post,
-            date: new Date(),
-            createdBy: this.userId,
-            likes:{
-                totalLikes:0,
-                users:[]
-            }
-        },
-        function(error, result){
-            if(error)console.log(error);
-            if(result)console.log(result);
+  Meteor.methods({
+    'insertPost' : function(post) {
+      Posts.insert({
+        post: post,
+        date: new Date(),
+        createdBy: this.userId,
+        likes: {
+          totalLikes: 0,
+          users: []
         }
-        );
+      },
+        function(error, result) {
+            if(error) console.log(error);
+            if(result) console.log(result);
+        }
+      );
     },
-    'likePost': function(postId){
-      console.log('boobs');
+    'likePost': function(postId) {
+      console.log('liked post');
       var update = true;
       Posts.update(
-      {_id:postId},{$addToSet : {"likes.users":this.userId}}),
-          function(error, result) {
-          if (error)
-              {
-                  update = false;
-              }
-          if (result)
-              {
-                  update = true;
-              }
+        { _id: postId },
+        { $addToSet: { "likes.users": this.userId } }
+      ),
+      function(error, result) {
+        if (error) {
+          update = false;
+        }
+        if (result) {
+          update = true;
+        }
       };
       if(update) (
         Posts.update(
-      {_id:postId}),
-      {$inc: {"likes.totalLikes":  1}}),
-                function (error, result){
+          { _id: postId }
+        ),
+        { $inc: {"likes.totalLikes": 1 } }),
+        function (error, result) {
           if (error) console.log(error);
           if (result) console.log(result);
-         }
-        },
-            'unlikePost':function(postId){
-                Posts.update({_id:postId},
-                    {$inc : {"likes.totalLikes": - 1}}),
-                    function (error, result){
-            if (error) console.log(error);
-            if (result) console.log(result);
-           };
-                Post.update(
-                {_id:postId},
-                    {$pop : {"likes.users" : this.userId}}
-                ),function (error, result){
-            if (error) console.log(error);
-            if (result) console.log(result);
-           };
-            },
-            'updatePost' :function(postObj) {
-              Posts.update({_id:postObj.id}, {$set: {post : postObj.post}});
-            }
-
-})
-    Meteor.publish('userPosts', function(){
-    return Posts.find();
-});
+        }
+      },
+    'unlikePost':function(postId) {
+      Posts.update(
+        { _id:postId },
+        { $inc : { "likes.totalLikes": - 1 } }
+      ),
+      function (error, result){
+        if (error) console.log(error);
+        if (result) console.log(result);
+      };
+      Post.update(
+        { _id: postId },
+        { $pop : { "likes.users" : this.userId } }
+      ),
+      function (error, result) {
+        if (error) console.log(error);
+        if (result) console.log(result);
+      };
+    },
+    'updatePost' : function(postObj) {
+      Posts.update({_id:postObj.id}, {$set: {post : postObj.post}});
+    }
+  })
+  Meteor.publish('userPosts', function(){
+      return Posts.find();
+  });
 });
